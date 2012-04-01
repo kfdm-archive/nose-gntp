90273c983c1765972548e588504fd9d2d6314301
Paul Traylor <kungfudiscomonkey@gmail.com>
2012-03-31 22:54:28 -0700

    More nose specific test suite

--- test.py
+++ test.py
@@ -1,7 +1,31 @@
-def test_true():
-    assert True
+import unittest
+from nose_gntp import NoseGrowl
+from nose.plugins import PluginTester
 
 
-def test_false():
-    assert False
+class TestSuccess(PluginTester, unittest.TestCase):
+    activate = '--with-growl'
+    plugins = [NoseGrowl()]
 
+    def test_success():
+        assert True
+
+    def makeSuite(self):
+        class TC(unittest.TestCase):
+            def runTest(self):
+                assert True
+        return unittest.TestSuite([TC()])
+
+
+class TestFailure(PluginTester, unittest.TestCase):
+    activate = '--with-growl'
+    plugins = [NoseGrowl()]
+
+    def test_failure():
+        assert False
+
+    def makeSuite(self):
+        class TC(unittest.TestCase):
+            def runTest(self):
+                assert False
+        return unittest.TestSuite([TC()])
